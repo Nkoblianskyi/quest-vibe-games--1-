2222bf8ad35d362858f1ecbc58b4fbcb1472e939
Nykyta Koblianskyi <nkoblianskyi@gmail.com>
2025-05-21 14:22:42 +0200

    12

--- next.config.mjs
+++ next.config.mjs
@@ -9,29 +9,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  headers: async () => [
-    {
-      source: "/(.*)",
-      headers: [
-        {
-          key: "Content-Security-Policy",
-          value: "default-src 'self'; img-src 'self' data:;",
-        },
-        {
-          key: "X-Content-Type-Options",
-          value: "nosniff",
-        },
-        {
-          key: "Referrer-Policy",
-          value: "no-referrer",
-        },
-        {
-          key: "X-Frame-Options",
-          value: "DENY",
-        },
-      ],
-    },
-  ],
 }
 
 export default nextConfig
